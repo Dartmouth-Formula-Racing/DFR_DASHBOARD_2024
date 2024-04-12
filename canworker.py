632608d179f24c4ba01f91dee801917b8c4b48e9
andreigerashchenko <67013056+andreigerashchenko@users.noreply.github.com>
2024-04-11 20:49:58 -0400

    Removed testing code, added CAN connection flag

--- canworker.py
+++ canworker.py
@@ -6,15 +6,26 @@
 # Useful documentation:
 # https://python-can.readthedocs.io/en/stable/interfaces/socketcand.html
 # https://python-can.readthedocs.io/en/stable/message.html
+# https://forum.peak-system.com/viewtopic.php?t=1267
 import can
+import time
+import RPi.GPIO
+import subprocess
+
+# MCP25625 pins
+nRST_GPIO = 5
+STBY_GPIO = 6
+# GPIO 25: Interrupt
 
 # Queues are passed to the worker process from the main process
 rx_queue = None
 tx_queue = None
 
-# Initialize CAN controller
-bus = can.interface.Bus(channel='can0', bustype='socketcan')
+# State dictionary is passed to the worker process from the main process
+state = None
 
+# CAN bus instance
+bus = None
 
 # CAN message format:
 # Byte 0: Bit 0 - Drive button state
@@ -23,17 +34,64 @@ bus = can.interface.Bus(channel='can0', bustype='socketcan')
 def build_message(drive_pressed, neutral_pressed, reverse_pressed):
     return can.Message(arbitration_id=0x102, data=[drive_pressed, neutral_pressed, reverse_pressed], extended_id=False)
 
-def can_worker(_rx_queue, _tx_queue):
+def can_worker(_rx_queue, _tx_queue, _state):
     global rx_queue
     global tx_queue
+    global state
+    global bus
 
     rx_queue = _rx_queue
     tx_queue = _tx_queue
 
+    # Configure GPIO for CAN controller and transceiver
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(nRST_GPIO, GPIO.OUT)
+    GPIO.setup(STBY_GPIO, GPIO.OUT)
+
+    # Set nRST high and STBY low
+    GPIO.output(nRST_GPIO, GPIO.HIGH)
+    GPIO.output(STBY_GPIO, GPIO.LOW)
+
+    # Configure CAN interface
+    try:
+        # Run command 'ip link set can0 type can restart-ms 10 bitrate 500000'
+        subprocess.run(['ip', 'link', 'set', 'can0', 'type', 'can', 'restart-ms', '10', 'bitrate', '500000'], check=True)
+    except subprocess.CalledProcessError:
+        print("Failed to configure CAN interface")
+        return
+    
+    # Bring up CAN interface
+    try:
+        # Run command 'ip link set up can0'
+        subprocess.run(['ip', 'link', 'set', 'up', 'can0'], check=True)
+    except subprocess.CalledProcessError:
+        print("Failed to bring up CAN interface")
+        return
+
+    # Main loop
     while True:
+        # Initialize socketcan interface
+        while bus is not None:
+            try:
+                bus = can.interface.Bus(bustype='socketcan', channel='can0')
+                state['canconnected'] = True
+            except:
+                bus = None
+                state['canconnected'] = False
+                print("CAN initialization failed, retrying...")
+                time.sleep(0.5)
+
+        # Check if bus is in error state
+        if bus.state == can.BusState.ERROR_ACTIVE:
+            print("CAN bus is in error state")
+            bus = None
+            state['canconnected'] = False
+            continue
+
         # Check for received data
-        if bus.recv():
-            rx_queue.put(bus.recv())
+        received = bus.recv(timeout=0.1)
+        if (received is not None):
+            rx_queue.put(received)
 
         # Check for data to send
         if not tx_queue.empty():
--- dashboard.py
+++ dashboard.py
@@ -6,8 +6,6 @@ import time
 # import canworker
 import webworker
 
-ANIMATE = True
-
 DRIVE_BUTTON_GPIO = 17
 DRIVE_LED_GPIO = 27
 NEUTRAL_BUTTON_GPIO = 24
@@ -37,143 +35,37 @@ if __name__ == '__main__':
     # Create a shared dictionary to store the data
     state = manager.dict()
 
-    # var faults_elm = document.getElementById('faults');
-    # var bot_elm = document.getElementById('bot');
-    # var brb_elm = document.getElementById('brb');
-    # var imd_elm = document.getElementById('imd');
-    # var bms_elm = document.getElementById('bms');
-    # var tsms_elm = document.getElementById('tsms');
-    # var drive_state_elm = document.getElementById('drive_state');
-
-    # var acctemp_elm = document.getElementById('acctemp');
-    # var leftinvtemp_elm = document.getElementById('leftinvtemp');
-    # var rightinvtemp_elm = document.getElementById('rightinvtemp');
-
-    # var throttlebar_elm = document.getElementById('throttlebar');
-    # var throttleval_elm = document.getElementById('throttleval');
-
-    # var rpm_elm = document.getElementById('rpm');
-    # var speed_elm = document.getElementById('speed');
-    # var lap_elm = document.getElementById('lap');
-    # var laptime_elm = document.getElementById('laptime');
-
-    # var batterybar_elm = document.getElementById('batterybar');
-    # var batteryval_elm = document.getElementById('batteryval');
-
     state['bot'] = False
     state['brb'] = False
     state['imd'] = False
     state['bms'] = False
     state['tsms'] = False
     state['drive_state'] = 'NEUTRAL'
-    state['acctemp'] = 150.0
-    state['leftinvtemp'] = 150.0
-    state['rightinvtemp'] = 150.0
-    state['throttle_position'] = 50.0
-    state['rpm'] = 5500.0
-    state['speed'] = 50.0
+    state['acctemp'] = 0.0
+    state['leftinvtemp'] = 0.0
+    state['rightinvtemp'] = 0.0
+    state['throttle_position'] = 0.0
+    state['rpm'] = 0.0
+    state['speed'] = 0.0
     state['lap'] = 0
     state['laptime'] = 0.0
-    state['battery_percentage'] = 50.0
-    state['accumulator_voltage'] = 250.0
-    state['LV_voltage'] = 50.0
-    state['accumulator_current'] = 150.0
-    state['accumulator_temperature'] = 150.0
-    state['estimated_range'] = 150.0
-    state['tractioncontrol'] = True
-    state['mileage'] = 1000.0
-    state['temperaturesok'] = True
+    state['battery_percentage'] = 0.0
+    state['accumulator_voltage'] = 0.0
+    state['LV_voltage'] = 0.0
+    state['accumulator_current'] = 0.0
+    state['accumulator_temperature'] = 0.0
+    state['estimated_range'] = 0.0
+    state['tractioncontrol'] = False
+    state['mileage'] = 0.0
+    state['temperaturesok'] = False
+    state['canconnected'] = False
 
     web_process = multiprocessing.Process(target=webworker.run, args=(state,), daemon=True)
-    # can_process = multiprocessing.Process(target=canworker.can_worker, args=(rx_queue, tx_queue), daemon=True)
+    # can_process = multiprocessing.Process(target=canworker.can_worker, args=(rx_queue, tx_queue, state,), daemon=True)
     web_process.start()
     # can_process.start()
 
-
-    increase = True
-
     while True:
-        if ANIMATE:
-
-            # Simulate data
-            if increase:
-                state['bot'] = True
-                state['brb'] = False
-                state['imd'] = True
-                state['bms'] = False
-                state['tsms'] = True
-                state['drive_state'] = 'REVERSE'
-                state['acctemp'] -= 1
-                state['leftinvtemp'] += 1
-                state['rightinvtemp'] -= 1
-                state['throttle_position'] += 1
-                state['rpm'] -= 10
-                state['speed'] += 1.0
-                state['lap'] -= 1
-                state['laptime'] += 1.0
-                state['battery_percentage'] -= 1
-                state['accumulator_voltage'] += 1
-                state['LV_voltage'] -= 1
-                state['accumulator_current'] += 1
-                state['accumulator_temperature'] -= 1
-                state['estimated_range'] += 1
-                state['tractioncontrol'] = False
-                state['mileage'] += 1.0
-                state['temperaturesok'] = False
-            else:
-                state['bot'] = False
-                state['brb'] = True
-                state['imd'] = False
-                state['bms'] = True
-                state['tsms'] = False
-                state['drive_state'] = 'DRIVE'
-                state['acctemp'] += 1
-                state['leftinvtemp'] -= 1
-                state['rightinvtemp'] += 1
-                state['throttle_position'] -= 1
-                state['rpm'] += 10
-                state['speed'] -= 1.0
-                state['lap'] += 1
-                state['laptime'] -= 1.0
-                state['battery_percentage'] += 1
-                state['accumulator_voltage'] -= 1
-                state['LV_voltage'] += 1
-                state['accumulator_current'] -= 1
-                state['accumulator_temperature'] += 1
-                state['estimated_range'] -= 1
-                state['tractioncontrol'] = True
-                state['mileage'] -= 1.0
-                state['temperaturesok'] = True
-            if state['speed'] >= 100:
-                increase = False
-            elif state['speed'] <= 0:
-                increase = True
-            time.sleep(1/100)
-        else:
-            state['bot'] = True
-            state['brb'] = True
-            state['imd'] = True
-            state['bms'] = True
-            state['tsms'] = True
-            state['drive_state'] = 'DRIVE'
-            state['acctemp'] = 100
-            state['leftinvtemp'] = 100
-            state['rightinvtemp'] = 100
-            state['throttle_position'] = 100
-            state['rpm'] = 100
-            state['speed'] = 100
-            state['lap'] = 100
-            state['laptime'] = 100
-            state['battery_percentage'] = 100
-            state['accumulator_voltage'] = 100
-            state['LV_voltage'] = 100
-            state['accumulator_current'] = 100
-            state['accumulator_temperature'] = 100
-            state['estimated_range'] = 100
-            state['tractioncontrol'] = True
-            state['mileage'] = 100
-            state['temperaturesok'] = True
-
         # Check button states
         # if GPIO.input(DRIVE_BUTTON_GPIO) == 0:
         #     drive_pressed = True
